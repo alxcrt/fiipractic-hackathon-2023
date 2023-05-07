363fe2831dd28134170823b6cf9f80728cdefcef
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2023-05-07 06:37:58 +0300

    Stickman animation

--- src/pages/selectLanguage.tsx
+++ src/pages/selectLanguage.tsx
@@ -1,9 +1,21 @@
 import LanguageSelector from "~/components/languageSelector";
+import Image from "next/image";
 
 const SelectLanguagePage = () => {
     return (
-        <div>
-            <LanguageSelector />
+        <div className="flex flex-col justify-center items-center min-h-screen pb-36">
+            <div className="mb-8">
+                <Image
+                    className="rounded-xl"
+                    src="/languages.jpg"
+                    alt="Rocket"
+                    width={450}
+                    height={1000}
+                />
+            </div>
+            <div>
+                <LanguageSelector />
+            </div>
         </div>
     );
 };
